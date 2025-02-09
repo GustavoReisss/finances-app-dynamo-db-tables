9a5b6ff304b0eceb3980bff0dbf836fbf9feb6a2
Gustavo Reis <gustavo.reis20000@gmail.com>
2025-02-09 10:02:50 -0300

    nova tabela de historicoDespesas

--- infra/tables.tf
+++ infra/tables.tf
@@ -55,6 +55,7 @@ resource "aws_dynamodb_table" "tipo_pagamento_table" {
   }
 }
 
+
 resource "aws_dynamodb_table" "despesa_table" {
   name         = "Despesa"
   billing_mode = "PAY_PER_REQUEST"
@@ -71,7 +72,47 @@ resource "aws_dynamodb_table" "despesa_table" {
     type = "S"
   }
 
+  attribute {
+    name = "dataProximoPagamento"
+    type = "S"
+  }
+
   tags = {
     Name = "Despesa"
+  }
+
+  global_secondary_index {
+    name               = "dataProximoPagamentoIndex"
+    hash_key           = "dataProximoPagamento"
+    projection_type    = "ALL"
+  }
+
+}
+
+resource "aws_dynamodb_table" "historico_despesa_table" {
+  name         = "HistoricoDespesa"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+  range_key    = "despesaId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "despesaId"
+    type = "S"
+  }
+
+  attribute {
+    name = "dataPagamento"
+    type = "S"
+  }
+
+  local_secondary_index {
+    name = "dataPagamentoIndex"
+    projection_type = "ALL"
+    range_key = "dataPagamento"
   }
 }
